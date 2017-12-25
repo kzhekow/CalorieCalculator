ca3cbbb435840d2d414c8c4001bd60741320f7aa
Alek Hristov <alek.hristov@yahoo.com>
2017-12-25 12:56:38 +0200

    Meal class implemented

--- CalorieCounterEngine/Models/Meals/Meal.cs
+++ CalorieCounterEngine/Models/Meals/Meal.cs
@@ -1,17 +1,25 @@
-﻿namespace CalorieCounterEngine.Models
+﻿using System;
+
+namespace CalorieCounterEngine.Models
 {
     using System.Collections.Generic;
     using global::CalorieCounterEngine.Contracts;
 
     public abstract class Meal : IMeal
     {
-        protected Meal(ICollection<IProduct> products)
+        private readonly ICollection<IProduct> products;
+        private readonly MealType type;
+
+        protected Meal(ICollection<IProduct> products, MealType type)
         {
-            // TODO: Validations
-            this.Products = products;
+            if (!Enum.IsDefined(typeof(MealType), type)) throw new ArgumentException("The provided meal type is not valid!");
+            this.products = products ?? throw new ArgumentNullException("You must add some products");
+            this.type = type;
         }
 
-        public ICollection<IProduct
-            > Products { get; }
+        public ICollection<IProduct> Products => this.products;
+
+        public MealType Type => throw new System.NotImplementedException();
+
     }
 }
