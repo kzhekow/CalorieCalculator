e9db9f01f578078c6466b9becab7c712f57d5ddb
NikolayN1kolov <nikolaytsnikolov@gmail.com>
2017-12-26 16:11:53 +0200

    Moved Factory class to Core Folder, created the Engine

---
+++ CalorieCounterEngine/Core/Engine/CalorieCalculatorEngine.cs
@@ -0,0 +1,25 @@
+﻿using CalorieCounterEngine.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CalorieCounterEngine.Core.Engine
+{
+    public sealed class CalorieCalculatorEngine : IEngine
+    {
+        private static readonly CalorieCalculatorEngine SingleInstance = new CalorieCalculatorEngine();
+
+        private CalorieCalculatorEngine()
+        {
+
+        }
+
+        public static CalorieCalculatorEngine Instance => SingleInstance;
+        
+
+        public ICommand CreateProductCommand => throw new NotImplementedException();
+    }
+}
