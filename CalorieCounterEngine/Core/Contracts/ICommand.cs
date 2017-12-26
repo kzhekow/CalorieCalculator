8c11593bce76b0049341086eb204a132ef4fca10
NikolayN1kolov <nikolaytsnikolov@gmail.com>
2017-12-26 16:32:03 +0200

    Moved Contracts to Core folder, created ICommand Interface

---
+++ CalorieCounterEngine/Core/Contracts/ICommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalorieCounterEngine.Core.Contracts
+{
+    interface ICommand
+    {
+        string Name { get; }
+
+        IList<string> Parameters { get; }
+    }
+}
