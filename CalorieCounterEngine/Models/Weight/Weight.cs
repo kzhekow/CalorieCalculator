35ad6aea6537c179b73f16c97b15742e7083a872
Alek Hristov <alek.hristov@yahoo.com>
2017-12-26 10:51:40 +0200

    Weight class implementation

---
+++ CalorieCounterEngine/Models/Weight/Weight.cs
@@ -0,0 +1,20 @@
+﻿using Bytes2you.Validation;
+using CalorieCounterEngine.Models.Contracts;
+
+namespace CalorieCounterEngine.Models.Weight
+{
+    /// <summary>
+    /// This class stores the individual body weight.
+    /// </summary>
+    public class Weight : IWeight
+    {
+        private readonly decimal weight;
+
+        public Weight(decimal weight)
+        {
+            Guard.WhenArgument(weight, "Weight can not be a negative nubmer!").IsLessThan(0).Throw();
+            this.weight = weight;
+        }
+        decimal IWeight.Weight => this.weight;
+    }
+}
