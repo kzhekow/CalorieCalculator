4a1b642b087a004ba54c0ffdb4918dd413cc3744
Alek Hristov <alek.hristov@yahoo.com>
2017-12-27 17:59:21 +0200

    IGoal interface implemented

---
+++ CalorieCounterEngine/Models/Contracts/IGoal.cs
@@ -0,0 +1,50 @@
+﻿namespace CalorieCounterEngine.Models.Contracts
+{
+    public interface IGoal
+    {
+        /// <summary>
+        ///     This is the weight of the user, when the goal was initially set. 
+        /// </summary>
+        double StartingWeight { get; }
+        /// <summary>
+        ///     This is the current weight of the user. 
+        /// </summary>
+        double CurrentWeight { get; }
+        /// <summary>
+        ///     This is the weight that the user is trying to reach. 
+        /// </summary>
+        double GoalWeight { get; }
+        /// <summary>
+        ///     Goals can be 3 types - lost weight, maintain weight and gain weight.
+        /// </summary>
+        GoalType Type { get; }
+        /// <summary>
+        ///     Calcultates the suggested macros ratio of the user, taking in mind his goal.
+        /// </summary>
+        /// <returns>
+        ///     Returns a string - "Carbs:Protein:Fat 50:20:30".
+        /// </returns>
+        string CalculateSuggestedMacrosRatio();
+        /// <summary>
+        ///     Calculates the suggested daily calorie intake of the user, taking in mind his goal.
+        /// </summary>
+        /// <returns>
+        ///     Returns integer of the number.
+        /// </returns>
+        int CalculateSuggestedDailyCalorieIntake();
+        /// <summary>
+        ///     Calculates the suggested daily water intake of the user.
+        /// </summary>
+        /// <returns>
+        ///     Returns integer of the number.
+        /// </returns>
+        int CalculateSuggestedWaterIntake();
+        /// <summary>
+        ///     Calculates resting calories of the user - energy his body expends just to keep him alive.
+        /// </summary>
+        /// <returns>
+        ///     Returns integer of the number.
+        /// </returns>
+        int CalculateRestingEnergy();
+    }
+}
