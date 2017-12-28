33ee5ac637267bd3514f70554924ffe7aa4ebad6
Alek Hristov <alek.hristov@yahoo.com>
2017-12-28 18:15:29 +0200

    CurrentDayCalorieTracker class updated members

--- CalorieCounterEngine/Models/CurrentDayCalorieTracker.cs
+++ CalorieCounterEngine/Models/CurrentDayCalorieTracker.cs
@@ -1,143 +1,147 @@
-﻿using Bytes2you.Validation;
-using CalorieCounter.Contracts;
-using CalorieCounterEngine.Models.Contracts;
-using System;
+﻿using CalorieCounter.Contracts;
+using CalorieCounter.Models.Contracts;
 using System.Collections.Generic;
 
 namespace CalorieCounterEngine.Models
 {
-    public class CurrentDayCalorieTracker : ICurrentDay, IProduct
+    public class CurrentDayCalorieTracker
     {
-        private double restingEnergy;
-        private double suggestedDailyCalorieIntake;
-        private int burnedCaloriesFromExercise;
+        public int Water { get; set; }
 
-        public CurrentDayCalorieTracker(double restingEnergy, double suggestedDailyCalorieIntake, int burnedCaloriesFromExercise = 0)
-        {
-            this.RestingEnergy = restingEnergy;
-            this.SuggestedDailyCalorieIntake = suggestedDailyCalorieIntake;
-            this.burnedCaloriesFromExercise = burnedCaloriesFromExercise;
-        }
+        public ICollection<IProduct> ProductsConsumed { get; set;}
 
-        public double RestingEnergy
-        {
-            get
-            {
-                return this.restingEnergy;
-            }
-            private set
-            {
-                Guard.WhenArgument(value, "Resting energy can not be a negative number!").IsLessThan(0).Throw();
-                this.restingEnergy = value;
-            }
-        }
+        public ICollection<IActivity> ActivitiesPerformed { get; set; }
 
-        public double SuggestedDailyCalorieIntake
-        {
-            get
-            {
-                return this.suggestedDailyCalorieIntake;
-            }
-            private set
-            {
-                Guard.WhenArgument(value, "Suggested daily calorie intake can not be a negative number!").IsLessThan(0).Throw();
-                this.suggestedDailyCalorieIntake = value;
-            }
-        }
+        //    private double restingEnergy;
+        //    private double suggestedDailyCalorieIntake;
+        //    private int burnedCaloriesFromExercise;
 
-        public int BurnedCaloriesFromExercise
-        {
-            get
-            {
-                return this.burnedCaloriesFromExercise;
-            }
-            private set
-            {
-                Guard.WhenArgument(value, "Burned calories from exercise can not be a negative number!").IsLessThan(0).Throw();
-                this.burnedCaloriesFromExercise = value;
-            }
-        }
+        //    public CurrentDayCalorieTracker(double restingEnergy, double suggestedDailyCalorieIntake, int burnedCaloriesFromExercise = 0)
+        //    {
+        //        this.RestingEnergy = restingEnergy;
+        //        this.SuggestedDailyCalorieIntake = suggestedDailyCalorieIntake;
+        //        this.burnedCaloriesFromExercise = burnedCaloriesFromExercise;
+        //    }
 
-        public ICollection<IProduct> Products => new List<IProduct>();
+        //    public double RestingEnergy
+        //    {
+        //        get
+        //        {
+        //            return this.restingEnergy;
+        //        }
+        //        private set
+        //        {
+        //            Guard.WhenArgument(value, "Resting energy can not be a negative number!").IsLessThan(0).Throw();
+        //            this.restingEnergy = value;
+        //        }
+        //    }
 
-        public ICollection<IMeal> Meals => new List<IMeal>();
+        //    public double SuggestedDailyCalorieIntake
+        //    {
+        //        get
+        //        {
+        //            return this.suggestedDailyCalorieIntake;
+        //        }
+        //        private set
+        //        {
+        //            Guard.WhenArgument(value, "Suggested daily calorie intake can not be a negative number!").IsLessThan(0).Throw();
+        //            this.suggestedDailyCalorieIntake = value;
+        //        }
+        //    }
 
-        public string Name => DateTime.Today.ToShortDateString();
+        //    public int BurnedCaloriesFromExercise
+        //    {
+        //        get
+        //        {
+        //            return this.burnedCaloriesFromExercise;
+        //        }
+        //        private set
+        //        {
+        //            Guard.WhenArgument(value, "Burned calories from exercise can not be a negative number!").IsLessThan(0).Throw();
+        //            this.burnedCaloriesFromExercise = value;
+        //        }
+        //    }
 
-        public int Calories { get; private set; }
+        //    public ICollection<IProduct> Products => new List<IProduct>();
 
-        public int Protein { get; private set; }
+        //    public ICollection<IMeal> Meals => new List<IMeal>();
 
-        public int Carbs { get; private set; }
+        //    public string Name => DateTime.Today.ToShortDateString();
 
-        public int Fat { get; private set; }
+        //    public int Calories { get; private set; }
 
-        public int Sugar { get; private set; }
+        //    public int Protein { get; private set; }
 
-        public int Fiber { get; private set; }
+        //    public int Carbs { get; private set; }
 
-        public double CalculateDailyIntake()
-        {
-            foreach (var product in this.Products)
-            {
-                this.Calories += product.Calories;
-                this.Protein += product.Protein;
-                this.Carbs += product.Carbs;
-                this.Fat += product.Fat;
-                this.Sugar += product.Sugar;
-                this.Fiber += product.Fiber;
-            }
+        //    public int Fat { get; private set; }
 
-            foreach (var meal in this.Meals)
-            {
-                foreach (var product in meal.Products)
-                {
-                    this.Calories += product.Calories;
-                    this.Protein += product.Protein;
-                    this.Carbs += product.Carbs;
-                    this.Fat += product.Fat;
-                    this.Sugar += product.Sugar;
-                    this.Fiber += product.Fiber;
-                }
-            }
+        //    public int Sugar { get; private set; }
 
-            return (this.SuggestedDailyCalorieIntake + this.BurnedCaloriesFromExercise) - this.Calories;
-        }
+        //    public int Fiber { get; private set; }
 
-        public string CalculateMacros()
-        {
-            var carbsRatio = Math.Round(((double)(this.Carbs * 4) / this.Calories) * 100);
-            var proteinRatio = Math.Round(((double)(this.Protein * 4) / this.Calories) * 100);
-            var fatRatio = Math.Round(((double)(this.Fat * 9) / this.Calories) * 100);
+        //    public double CalculateDailyIntake()
+        //    {
+        //        foreach (var product in this.Products)
+        //        {
+        //            this.Calories += product.Calories;
+        //            this.Protein += product.Protein;
+        //            this.Carbs += product.Carbs;
+        //            this.Fat += product.Fat;
+        //            this.Sugar += product.Sugar;
+        //            this.Fiber += product.Fiber;
+        //        }
 
-            return $"Carbs:Protein:Fat = {carbsRatio}:{proteinRatio}:{fatRatio}";
-        }
+        //        foreach (var meal in this.Meals)
+        //        {
+        //            foreach (var product in meal.Products)
+        //            {
+        //                this.Calories += product.Calories;
+        //                this.Protein += product.Protein;
+        //                this.Carbs += product.Carbs;
+        //                this.Fat += product.Fat;
+        //                this.Sugar += product.Sugar;
+        //                this.Fiber += product.Fiber;
+        //            }
+        //        }
 
-        public void AddProducts(IProduct product)
-        {
-            Guard.WhenArgument(product, "Product can not be null!").IsNull().Throw();
-            this.Products.Add(product);
-        }
+        //        return (this.SuggestedDailyCalorieIntake + this.BurnedCaloriesFromExercise) - this.Calories;
+        //    }
 
-        public void RemoveProducts(IProduct product)
-        {
-            Guard.WhenArgument(product, "Product can not be null!").IsNull().Throw();
-            Guard.WhenArgument(this.Products, "The list of products is empty!").IsNull().Throw();
+        //    public string CalculateMacros()
+        //    {
+        //        var carbsRatio = Math.Round(((double)(this.Carbs * 4) / this.Calories) * 100);
+        //        var proteinRatio = Math.Round(((double)(this.Protein * 4) / this.Calories) * 100);
+        //        var fatRatio = Math.Round(((double)(this.Fat * 9) / this.Calories) * 100);
 
-            this.Products.Remove(product);
-        }
+        //        return $"Carbs:Protein:Fat = {carbsRatio}:{proteinRatio}:{fatRatio}";
+        //    }
 
-        public void AddMeal(IMeal meal)
-        {
-            Guard.WhenArgument(meal, "Meal can not be null!").IsNull().Throw();
-            this.Meals.Add(meal);
-        }
+        //    public void AddProducts(IProduct product)
+        //    {
+        //        Guard.WhenArgument(product, "Product can not be null!").IsNull().Throw();
+        //        this.Products.Add(product);
+        //    }
 
-        public void RemoveMeal(IMeal meal)
-        {
-            Guard.WhenArgument(meal, "Meal can not be null!").IsNull().Throw();
-            Guard.WhenArgument(this.Meals, "The list of meals is empty!").IsNull().Throw();
-            this.Meals.Remove(meal);
-        }
+        //    public void RemoveProducts(IProduct product)
+        //    {
+        //        Guard.WhenArgument(product, "Product can not be null!").IsNull().Throw();
+        //        Guard.WhenArgument(this.Products, "The list of products is empty!").IsNull().Throw();
+
+        //        this.Products.Remove(product);
+        //    }
+
+        //    public void AddMeal(IMeal meal)
+        //    {
+        //        Guard.WhenArgument(meal, "Meal can not be null!").IsNull().Throw();
+        //        this.Meals.Add(meal);
+        //    }
+
+        //    public void RemoveMeal(IMeal meal)
+        //    {
+        //        Guard.WhenArgument(meal, "Meal can not be null!").IsNull().Throw();
+        //        Guard.WhenArgument(this.Meals, "The list of meals is empty!").IsNull().Throw();
+        //        this.Meals.Remove(meal);
+        //    }
     }
 }
