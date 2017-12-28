682d025cbcb335ec3aa31ede87277c5625aff395
Alek Hristov <alek.hristov@yahoo.com>
2017-12-28 19:18:25 +0200

    Added RemainingDailyWaterIntake method

--- CalorieCounterEngine/Models/DailyNutriCalc.cs
+++ CalorieCounterEngine/Models/DailyNutriCalc.cs
@@ -6,11 +6,11 @@ namespace CalorieCounter.Models
 {
     public static class DailyNutriCalc
     {
-
         public static int CalculateCurrentSugars(ICollection<IProduct> productsConsumed)
         {
             return productsConsumed.Sum(x => x.Sugar);
         }
+
         public static int CalculateCurrentFibers(ICollection<IProduct> productsConsumed)
         {
             return productsConsumed.Sum(x => x.Fiber);
@@ -22,22 +22,30 @@ namespace CalorieCounter.Models
 
             return suggestedProteinDailyIntake - currentProteinIntake;
         }
+
         public static double RemainingCarbsIntake(double suggestedCarbsDailyIntake, ICollection<IProduct> productsConsumed)
         {
             double currentCarbIntake = productsConsumed.Sum(x => x.Carbs);
 
             return suggestedCarbsDailyIntake - currentCarbIntake;
         }
+
         public static double RemainingFatsIntake(double suggestedFatsDailyIntake, ICollection<IProduct> productsConsumed)
         {
             double currentFatIntake = productsConsumed.Sum(x => x.Fat);
             return suggestedFatsDailyIntake - currentFatIntake;
         }
+
         public static int RemainingCaloriesIntake(int suggestedCaloriesDailyIntake, ICollection<IProduct> productsConsumed)
         {
             int currentCaloriesIntake = productsConsumed.Sum(x => x.Calories);
 
             return suggestedCaloriesDailyIntake - currentCaloriesIntake;
+        }
+
+        public static int RemainingWaterIntake(int suggestedDailyWaterIntake, int waterConsumed)
+        {
+            return suggestedDailyWaterIntake - waterConsumed;
         }
         //public static int CalculateCurrentCalories(ICollection<IProduct> productsConsumed)
         //{
