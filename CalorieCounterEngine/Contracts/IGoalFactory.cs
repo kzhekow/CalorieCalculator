8109dc6572b9b8e298445189ce53dbb417e35f8f
Alek Hristov <alek.hristov@yahoo.com>
2017-12-30 18:32:09 +0200

    IGoalFactory implemented

---
+++ CalorieCounterEngine/Contracts/IGoalFactory.cs
@@ -0,0 +1,9 @@
+﻿using CalorieCounterEngine.Models.Contracts;
+
+namespace CalorieCounterEngine.Contracts
+{
+    public interface IGoalFactory
+    {
+        IGoal CreateGoal(double startingWeight, double goalWeight, double height, int age, GenderType gender, GoalType type, ActivityLevel level);
+    }
+}
