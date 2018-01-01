c71650564a991f6a83e2610c592d8f693cccc91c
Nikolay Nikolov <nikolaytsnikolov@gmail.com>
2018-01-02 00:10:32 +0200

    Adding AddWaterCommand

--- Console_App/Core/Commands/Adding/AddWaterCommand.cs
+++ Console_App/Core/Commands/Adding/AddWaterCommand.cs
@@ -1,6 +1,41 @@
-﻿namespace Console_App.Core.Commands.Adding
+﻿using CalorieCounter.Contracts;
+using Console_App.Core.Contracts;
+using System;
+using System.Collections.Generic;
+
+namespace Console_App.Core.Commands.Adding
 {
-    internal class AddWaterCommand
+    internal class AddWaterCommand : ICommand
     {
+        public AddWaterCommand(ICalorieCounterEngine calorieCounterEngine)
+        {
+            this.CalorieCounterEngine = calorieCounterEngine;
+        }
+
+        protected ICalorieCounterEngine CalorieCounterEngine { get; }
+
+        public string Execute(IList<string> parameters)
+        {
+            int waterVolume;
+
+            try
+            {
+                waterVolume = int.Parse(parameters[0]);
+                
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("Failed to parse AddActivity command parameters.");
+            }
+
+            object[] args = { waterVolume };
+
+            if (this.CalorieCounterEngine.AddWaterCommand.CanExecute(args))
+            {
+                this.CalorieCounterEngine.AddWaterCommand.Execute(args);
+            }
+
+            return $"{waterVolume}ml was added to your daily water consumption";
+        }
     }
 }
