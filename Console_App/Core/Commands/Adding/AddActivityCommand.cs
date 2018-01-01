6ba472b5ddb25ca4d90bdc611f630b7b363e3be5
Nikolay Nikolov <nikolaytsnikolov@gmail.com>
2018-01-02 00:06:41 +0200

    Adding AddActivityCommand

--- Console_App/Core/Commands/Adding/AddActivityCommand.cs
+++ Console_App/Core/Commands/Adding/AddActivityCommand.cs
@@ -1,6 +1,43 @@
-﻿namespace Console_App.Core.Commands.Adding
+﻿using CalorieCounter.Contracts;
+using Console_App.Core.Contracts;
+using System;
+using System.Collections.Generic;
+
+namespace Console_App.Core.Commands.Adding
 {
-    internal class AddActivityCommand
+    internal class AddActivityCommand : ICommand
     {
+ 
+        public AddActivityCommand(ICalorieCounterEngine calorieCounterEngine)
+        {
+            this.CalorieCounterEngine = calorieCounterEngine;
+        }
+
+        protected ICalorieCounterEngine CalorieCounterEngine { get; }
+
+        public string Execute(IList<string> parameters)
+        {
+            string activityType;
+            int time;
+
+            try
+            {
+                activityType = parameters[0];
+                time = int.Parse(parameters[1]);
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("Failed to parse AddActivity command parameters.");
+            }
+
+            object[] args = { activityType, time };
+
+            if (this.CalorieCounterEngine.AddConsumedProductCommand.CanExecute(args))
+            {
+                this.CalorieCounterEngine.AddConsumedProductCommand.Execute(args);
+            }
+
+            return $"Your activity was added";
+        }
     }
 }
