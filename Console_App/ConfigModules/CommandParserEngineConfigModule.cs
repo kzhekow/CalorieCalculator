58eb939ec3539e09cbe5230e61bcfcc1d94d5b3c
Alek Hristov <alek.hristov@yahoo.com>
2018-02-01 16:06:59 +0200

    ConfigModules added

---
+++ Console_App/ConfigModules/CommandParserEngineConfigModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App.ConfigModules
+{
+    public class CommandParserEngineConfigModule : Module
+    {
+
+    }
+}
