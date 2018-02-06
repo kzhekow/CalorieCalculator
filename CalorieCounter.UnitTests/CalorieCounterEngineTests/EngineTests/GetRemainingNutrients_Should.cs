2505bc48f20229a648107a68de59a6074918cf0f
Alek Hristov <alek.hristov@yahoo.com>
2018-02-06 11:53:31 +0200

    New unit tests added

---
+++ CalorieCounter.UnitTests/CalorieCounterEngineTests/EngineTests/GetRemainingNutrients_Should.cs
@@ -0,0 +1,31 @@
+﻿using CalorieCounter.Factories.Contracts;
+using CalorieCounterEngine.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace CalorieCounter.UnitTests.CalorieCounterEngineTests.EngineTests
+{
+    [TestClass]
+    public class GetRemainingNutrients_Should
+    {
+        [TestMethod]
+        public void ReturnGoalNotSetMessage_GoalHadNotBeenSet()
+        {
+            // Assert
+            var productFactoryMock = new Mock<IProductFactory>();
+            var activityFactoryMock = new Mock<IActivityFactory>();
+            var goalFactoryMock = new Mock<IGoalFactory>();
+            var dailyNutriCalcMock = new Mock<IDailyNutriCalc>();
+            var expectedMessage = "Goal has not been set!";
+
+            var engine = new Engine
+                (productFactoryMock.Object,
+                activityFactoryMock.Object,
+                goalFactoryMock.Object,
+                dailyNutriCalcMock.Object);
+
+            // Act && Assert
+            Assert.AreEqual(expectedMessage, engine.GetRemainingNutrients());
+        }
+    }
+}
--- CalorieCounterEngine/Contracts/IEngine.cs
+++ CalorieCounterEngine/Contracts/IEngine.cs
@@ -5,14 +5,23 @@ namespace CalorieCounter.Contracts
     public interface IEngine
     {
         ICommand CreateFoodProductCommand { get; }
+
         ICommand CreateDrinkCommand { get; }
+
         ICommand CreateMealCommand { get; }
+
         ICommand SetGoalCommand { get; }
+
         ICommand AddConsumedProductCommand { get; }
+
         ICommand AddWaterCommand { get; }
+
         ICommand AddActivityCommand { get; }
+
         ICommand GetAllProductsCommand { get; }
+
         string GetDailyReport();
+
         string GetRemainingNutrients();
     }
 }
